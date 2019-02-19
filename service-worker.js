f0eb357a39969572dc1acd1e7dc51616e824241c
bryon <bryon@data.world>
2019-02-19 08:36:29 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ddw-react-oauth-start/precache-manifest.41ec25ebe25aa724fe7ff788bde068da.js"
+  "/ddw-react-oauth-start/precache-manifest.3595e5d41eeeac89a34e81c7e26180c6.js"
 );
 
 workbox.clientsClaim();
